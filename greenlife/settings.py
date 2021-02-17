4ae1a9cd3ba6747b8ad1a6af52b7134b6aca6ae0
Tamzid Ahmed <tamjidahmed958@gmail.com>
2021-02-17 21:20:31 +0600

    project review done

--- greenlife/settings.py
+++ greenlife/settings.py
@@ -50,7 +50,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'greenlife.urls'
@@ -122,8 +121,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # media directory in the root directory
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
