4fce27fd960490a22f20fb8fe1a46fd8a27c7313
Tamzid Ahmed <tamjidahmed958@gmail.com>
2021-02-17 21:09:39 +0600

    project review done

--- Procfile
+++ Procfile
@@ -1,2 +1,4 @@
 heroku ps:scale web=1
-web: gunicorn greenlife.wsgi
+web: gunicorn greenlife.wsgi:application --log-file - --log-level debug
+python manage.py collectstatic --noinput
+manage.py migrate
--- greenlife/settings.py
+++ greenlife/settings.py
@@ -50,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'greenlife.urls'
@@ -121,6 +122,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # media directory in the root directory
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
